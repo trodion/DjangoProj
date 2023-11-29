209f38a5265526b0d8393728ae87d66e824ebc4c
trodion <rodion.toporoff@yandex.ru>
2023-11-29 09:14:44 +0300

    first commit

---
+++ ecom/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from .models import Customer,Product,Orders,Feedback, News
+# Register your models here.
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Customer, CustomerAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Product, ProductAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Orders, OrderAdmin)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Feedback, FeedbackAdmin)
+
+class NewsAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(News, FeedbackAdmin)
+# Register your models here.
